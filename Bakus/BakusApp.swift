c5cc8d9f441ff1b5fe1cad9d6e9429a931284e19
Edward Seim <sirseim@gmail.com>
2022-12-20 14:28:33 -0600

    start of the new project

---
+++ Bakus/BakusApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakusApp.swift
+//  Bakus
+//
+//  Created by Edward Seim on 12/20/22.
+//
+
+import SwiftUI
+
+@main
+struct BakusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
